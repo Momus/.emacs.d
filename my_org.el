f4e2c22f1bee7631cad9dd518bc5ef650d0047d3
Dmitri Brengauz <dmitri@momus.net>
2012-10-02 16:58:08 -0600

    eshel put in some aliases

--- my_org.el
+++ my_org.el
@@ -2,16 +2,17 @@
 ;;  http://orgmode.org/worg/org-tutorials/orgtutorial_dto.html
 
 (require 'org-install)
- (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
- (define-key global-map "\C-cl" 'org-store-link)
- (define-key global-map "\C-ca" 'org-agenda)
- (setq org-log-done t)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(global-font-lock-mode 1)
 
 
-;; Something about setting vis line mode is wrong
-;; but I don't feel like changing it.
+;; Setting the  vis line mode
 
 (add-hook 'org-mode-hook
 	  '(lambda ()
 	     (auto-fill-mode nil)
 	     (visual-line-mode t)))
+
