6bf8c4891328c39a6c1b6e8e20b53c46979a01ba
Dmitri Brengauz <dmitri@momus.net>
2012-10-02 20:20:44 -0600

    faliure of adding an eshell module

--- init.el
+++ init.el
@@ -1,10 +1,20 @@
 ;; This is the main init file.  It should get split up as it gets bigger.
 
+
+;; add the directory in which are kept various elsips loaded below.
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
 ;; Load the org mode configuration
 (load "my_org")
 
 
 ;;Some basic config options
+
+
+;A steady light, not blinking parens
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+(setq show-paren-delay 0)
 
 ;No splash screen at start-up
 (setq inhibit-startup-screen t)
@@ -13,3 +23,8 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
+
+;; When I configure more eshell stuff, it will get it's own file
+
+; TODO:   em-last.el --- insert arguments from previous commands
+;;(add-to-list 'eshell-modules-list "eshell-last")
