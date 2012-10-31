fef9100dc56a44b9dbf4ed336faf73ba7195a9f5
kwaku <kwaku@Sticky.momus.org>
2012-10-31 12:35:23 -0600

    added a little ruby magic

--- rubies.el
+++ rubies.el
@@ -10,3 +10,30 @@
 
 (add-hook 'ruby-mode-hook
           (lambda () (rvm-activate-corresponding-ruby)))
+
+;;Improving Ruby Mode
+
+;;Ruby-mode file associations:
+;;http://stackoverflow.com/questions/13165811/enabling-ruby-mode-for-sundry-rails-files
+(add-to-list 'auto-mode-alist '("\\.\\(rb\\|ru\\|builder\\|rake\\|thor\\|gemspec\\)\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\(rake\\|thor\\|guard\\|gem\\|cap\\|vagrant\\)file\\'" . ruby-mode))
+
+
+; auto-indent the next line after you hit return
+;http://appsintheopen.com/articles/1-setting-up-emacs-for-rails-development/part/7-emacs-ruby-foo
+
+(add-hook 'ruby-mode-hook
+      (lambda()
+        (add-hook 'local-write-file-hooks
+                  '(lambda()
+                     (save-excursion
+                       (untabify (point-min) (point-max))
+                       (delete-trailing-whitespace)
+                       )))
+        (set (make-local-variable 'indent-tabs-mode) 'nil)
+        (set (make-local-variable 'tab-width) 2)
+        (imenu-add-to-menubar "IMENU")
+        (define-key ruby-mode-map "\C-m" 'newline-and-indent) ;Not sure if this line is 100% right!
+     ;   (require 'ruby-electric)
+     ;   (ruby-electric-mode t)
+        ))
