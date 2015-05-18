1d4a14770181b96acbd67a4dfeffd56eb989b8ee
Dmitri G. Brengauz <dmitri@momus.net>
2015-05-17 21:06:40 -0600

    Got rid of stupid warning message for Magit. Enabled M-s M-s as global magit status

--- elisp/gits.el
+++ elisp/gits.el
@@ -2,11 +2,11 @@
 
 ;; Magit.  Download from [[https://github.com/magit/magit][github]] and build
 
-;(require 'magit)
+(require 'magit)
 
 ;;Globally bind  magit status
-;(global-set-key 
-; (kbd "M-s M-s")  'magit-status)
+(global-set-key 
+ (kbd "M-s M-s")  'magit-status)
 
-
-
+;; Keep warning message from appearing on start up.  Kinda dumb.
+ (setq magit-last-seen-setup-instructions "1.4.0")
